70172ce27b6b76e82eff743da999d95404a3ae14
Ebenézer Rangel Botelho <bene20@gmail.com>
2023-04-24 12:33:34 -0300

    Commit inicial - Projeto java criado no Netbeans 17

---
+++ src/main/java/br/com/bene20/cursospringexpert/Cursospringexpert.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.bene20.cursospringexpert;
+
+/**
+ *
+ * @author ebenezer.botelho
+ */
+public class Cursospringexpert {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
