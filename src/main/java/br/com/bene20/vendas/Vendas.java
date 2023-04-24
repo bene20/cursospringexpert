f3712091a7af036d76a8c22ac299e3b814ce1e22
Ebenézer Rangel Botelho <bene20@gmail.com>
2023-04-24 12:39:31 -0300

    Projeto renomeado para Vendas

--- src/main/java/br/com/bene20/vendas/Vendas.java
+++ src/main/java/br/com/bene20/vendas/Vendas.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package br.com.bene20.cursospringexpert;
+package br.com.bene20.vendas;
 
 /**
  *
  * @author ebenezer.botelho
  */
-public class Cursospringexpert {
+public class Vendas {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
