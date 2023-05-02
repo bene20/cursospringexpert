744f8cb348a7c1dfab9f2e5f9d65e13c133829a8
Ebenézer Rangel Botelho <bene20@gmail.com>
2023-05-02 15:18:14 -0300

    CRUD com JDBC direto

--- src/main/java/br/com/bene20/vendas/VendasApplication.java
+++ src/main/java/br/com/bene20/vendas/VendasApplication.java
@@ -1,11 +1,47 @@
 package br.com.bene20.vendas;
 
+import br.com.bene20.vendas.domain.entity.Cliente;
+import br.com.bene20.vendas.domain.repositorio.Clientes;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
 
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            clientes.salvar(new Cliente("Pedro"));
+            clientes.salvar(new Cliente("Ana"));
+            clientes.salvar(new Cliente("Caio"));
+            
+            System.out.println("1 - Relação de todos os nomes:");
+            clientes.obterTodos().forEach(System.out::println );
+            
+            System.out.println("2 - Alterando todos os nomes:");
+            clientes.obterTodos().forEach(c -> {
+                c.setNome(c.getNome() + " atualizado");
+                clientes.atualizar(c);
+            });
+            clientes.obterTodos().forEach(System.out::println);
+            
+            System.out.println("3 - Pequisando cliente %na%:");
+            clientes.buscarPorNome("na").forEach(System.out::println );
+
+//            System.out.println("4 - Excluindo os clientes:");
+//            clientes.obterTodos().forEach(c -> {
+//                clientes.deletar(c);
+//            });
+
+            System.out.println("5 - Listagem final dos clientes:");
+            clientes.obterTodos().forEach(System.out::println);;
+        };
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
